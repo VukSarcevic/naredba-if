1a624641ff4e528a6e3f337967018700edc6c4a3
VukSarcevic <113903379+VukSarcevic@users.noreply.github.com>
2022-09-19 14:37:13 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-let a = 3;
-a = a % 3;
-let b = a;
-b = b + 1;
-console.log(a);
-console.log(b);
+// let a = 3;
+// a = a % 3;
+// let b = a;
+// b = b + 1;
+// console.log(a);
+// console.log(b);
+// let a = prompt("Unesite prvi broj");
+// let b = prompt("Unesite drugi broj");
+// console.log(`Zbir brojeva je ${Number(a) + Number(b)}`);
+let a = prompt("Unesite broj");
+if (a > 100)
+{
+  console.log("broj je veci od 100");
+}
+else
+{
+  console.log("broj je manji od 100");
+}
